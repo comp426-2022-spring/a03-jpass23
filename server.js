95dfebd5f0a1d076babf070847712a93f9b81ee2
jpass23 <jpass23@live.unc.edu>
2022-02-25 11:53:37 -0500

    required dependencies

---
+++ server.js
@@ -0,0 +1,10 @@
+// Require Express.js
+const express = require('express')
+const app = express()
+
+//import minimist
+import minimist from "minimist";
+const args = minimist(process.argv.slice(2));
+// const minimist = require('minimist')
+// const args = minimist()
+
