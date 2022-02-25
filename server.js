ad657006c61c68ac6df177e03292e73f87bcbf80
jpass23 <jpass23@live.unc.edu>
2022-02-25 11:58:49 -0500

    Server and default endpoint created

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const express = require('express')
 const app = express()
 
 //import minimist
-import minimist from "minimist";
+const minimist = require('minimist')
 const args = minimist(process.argv.slice(2));
-// const minimist = require('minimist')
 // const args = minimist()
 
+var HTTP_PORT = 5000
+// Start an app server
+const server = app.listen(HTTP_PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+});
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
