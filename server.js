3dff06de91269711e1337b3dc7793759ee9e26ab
jpass23 <jpass23@live.unc.edu>
2022-02-26 13:09:41 -0500

    API up and running

--- server.js
+++ server.js
@@ -5,15 +5,30 @@ const app = express()
 //import minimist
 const minimist = require('minimist')
 const args = minimist(process.argv.slice(2));
-// const args = minimist()
 
-var HTTP_PORT = 5000
+var HTTP_PORT = args['port'] || 5000
+
 // Start an app server
 const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
 });
 
+// Setting Endpoint
+app.get('/app/', (req, res) => {
+// Respond with status 200
+    res.statusCode = 200;
+// Respond with status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+    res.end(res.statusCode+ ' ' +res.statusMessage)
+});
+
+app.get('/app/flips/:number', (req, res) => {
+	res.send(req.params.number)
+});
+
 // Default response for any other request
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
+    res.type("text/plain")
 });
