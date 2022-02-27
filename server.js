3caf4d79879ba253e1a345bce44cbb3b3f8469ae
jpass23 <jpass23@live.unc.edu>
2022-02-27 17:53:31 -0500

    /app/flip/ done

--- server.js
+++ server.js
@@ -1,3 +1,48 @@
+//FUNCTIONS//
+
+function coinFlip() {
+    return Math.floor(Math.random()*2) ? 'heads' : 'tails';
+}
+
+function coinFlips(flips) {
+    var arr = []
+    for (let i = 0; i<flips; i++){
+      arr[i] = coinFlip(); 
+    }
+    return arr;
+}
+
+function countFlips(array) {
+    const counter = {
+      heads: 0,
+      tails: 0,
+    }
+    array.forEach(element => {
+      (element=='heads') ? counter.heads += 1 : counter.tails += 1;
+    });
+    if(counter.heads == 0){
+      return {tails: counter.tails};
+    }
+    if(counter.tails == 0){
+      return {heads: counter.heads};
+    }
+    return counter
+}
+
+function flipACoin(call) {
+    const object = {
+      call: '',
+      flip: '',
+      result: '',
+    }
+    object.call = call;
+    object.flip = coinFlip();
+    (object.flip == object.call) ? object.result = 'win' : object.result = 'lose';
+    return object;
+}
+
+//SERVER//
+
 // Require Express.js
 const express = require('express')
 const app = express()
@@ -23,12 +68,17 @@ app.get('/app/', (req, res) => {
     res.end(res.statusCode+ ' ' +res.statusMessage)
 });
 
+app.get('/app/flip/', (req, res) => {
+    res.status(200).type('text/json');
+	res.json({flip: coinFlip()})
+});
+
 app.get('/app/flips/:number', (req, res) => {
 	res.send(req.params.number)
 });
 
 // Default response for any other request
 app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
+    res.status(404).send('404 Not found')
     res.type("text/plain")
 });
